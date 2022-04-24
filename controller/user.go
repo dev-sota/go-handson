6a0bbec7067159fc9eccef88f9dbd6e0cbed9468
Satoshi Ota <sota.developer@gmail.com>
2022-04-24 16:28:54 +0900

    Add example logic

--- controller/user.go
+++ controller/user.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -28,6 +29,41 @@ func (u user) Get(w http.ResponseWriter, r *http.Request) {
 	}
 
 	v := view.NewUser(m)
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(v)
+}
+
+func (u user) Signup(w http.ResponseWriter, r *http.Request) {
+	name := "dev-sota"
+	email := "test@exam.com"
+
+	var m *model.User
+	if err := m.FindByEmail(email); err != nil {
+		return
+	}
+	if m.ID != 0 {
+		fmt.Println("already registerd")
+		w.Header().Add("Content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	// ビジネスロジック
+	// if name == "ito" {
+	// 	fmt.Println("クーポン発行")
+	// }
+	// if age < 20 {
+	// 	fmt.Println("クーポン発行")
+	// }
+	// if birthDayMonth == time.Now().Month() {
+	// 	fmt.Println("クーポン発行")
+	// }
+
+	usr := model.NewUser(name, email)
+	usr.Create()
+
+	v := view.NewUser(usr)
 
 	w.Header().Add("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
